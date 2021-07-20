752233b5b90c12ca5e00516cbf311397c43f4c1e
jeondoh <41669026+jeondoh@users.noreply.github.com>
2021-07-20 23:45:20 +0900

    Level develop

---
+++ Source/UE4_UltimateGame/public/FloorSwitch.h
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "FloorSwitch.generated.h"
+
+UCLASS()
+class UE4_ULTIMATEGAME_API AFloorSwitch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloorSwitch();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switch")
+	UBoxComponent* TriggerBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switch")
+	UStaticMeshComponent* FloorSwitch;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switch")
+	UStaticMeshComponent* Door;
+
+	UPROPERTY(BlueprintReadWrite, Category="Floor Switch")
+	FVector InitalDoorLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category="Floor Switch")
+	FVector InitalSwitchLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Floor Switch")
+	float SwitchTime;
+	
+	FTimerHandle SwitchHandle;
+
+	bool bCharacterOnSwitch;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void RaiseDoor();
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void LowerDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void RaiseFloorSwitch();
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void LowerFloorSwitch();
+
+	UFUNCTION(BlueprintCallable, Category="Floor Switch")
+	void UpdateDoorLocation(float z);
+
+	UFUNCTION(BlueprintCallable, Category="Floor Switch")
+	void UpdateFloorSwitchLocation(float z);
+	
+	void CloseDoor();
+};
---
+++ Source/UE4_UltimateGame/public/SpawnVolume.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class UE4_ULTIMATEGAME_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawning")
+	UBoxComponent* SpawningBox;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
